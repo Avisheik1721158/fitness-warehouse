6e728b4aebe4f1a64740279ec48092deae15e816
Avisheikbarua1158 <avisheik.barua@northsouth.edu>
2022-05-05 21:11:06 +0600

    home page initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-100">
+      <Header></Header>
+
+      <Routes>
+        <Route path="/" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" element={<Home></Home>}></Route>
+        <Route path="/about" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" element={<About></About>}></Route>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+
+import { Link } from 'react-router-dom';
+// import { MenuIcon, XIcon } from '@heroicons/react/solid'
+
+
+const Header = () => {
+    // const [open, setOpen] = useState(false);
+    return (
+        <div>
+
+            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+                <div>
+
+                </div>
+                <div className="container flex flex-wrap justify-between items-center mx-auto">
+                    <Link to="/" className="flex items-center">
+                        {/* <img src="/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"> */}
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Fitness Warehouse</span>
+                    </Link>
+
+                    {/* <div className=" hidden w-full md:flex md:w-auto" id="mobile-menu"> */}
+                    <div>
+
+
+                        {/* <div onClick={() => setOpen(!open)} >{open ? <XIcon className='w-6 h-6 fill-orange-900 lg:hidden mx-auto'></XIcon> : <MenuIcon className='w-6 h-6 fill-orange-900 lg:hidden mx-auto'></MenuIcon>}</div> */}
+                        <ul className='flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium'>
+                            <li>
+                                <Link to="/" className="block py-2 pr-4 pl-3 text-white bg-red-700 rounded md:bg-transparent md:text-red-700 md:p-0 dark:text-white" >Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >About</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    );
+};
+
+export default Header;
